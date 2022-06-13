4d8f4cb22f7887b8b3327afa647262d28951460d
alexandre-notor <amanichambu48@gmail.com>
2022-06-13 20:48:26 +0200

    feat: add modal function to the page

--- modal/script.js
+++ modal/script.js
@@ -18,3 +18,20 @@ icon.addEventListener('click', () => {
    afficherMenu()
 });
 
+/*
+    YOUR SCRIPT 
+*/ 
+let buttons = [document.querySelector("#open"), document.querySelector("#close")];
+let hidelayer = document.querySelector("#hidelayer");
+let mymodal = document.querySelector("#mymodal");
+function toggle(todo) {
+    if(todo.target == buttons[0]) {
+        hidelayer.style.display = "block";
+        mymodal.style.display = "block";
+    } else {
+        hidelayer.style.display = "none";
+        mymodal.style.display = "none";
+    }
+}
+buttons[0].addEventListener('click', toggle)
+buttons[1].addEventListener('click', toggle)
