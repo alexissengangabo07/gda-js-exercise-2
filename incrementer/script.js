c15045ac712bdd333c03f5175199649e312e6599
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-07-14 16:24:18 +0200

    factory-button increment and decrement

--- incrementer/script.js
+++ incrementer/script.js
@@ -18,3 +18,34 @@ icon.addEventListener('click', () => {
    afficherMenu()
 });
 
+const decrement = document.querySelector(".decrement");
+const reset = document.querySelector(".reset");
+const increse = document.querySelector(".increse");
+// const counter = document.querySelector(".counter-value");
+const counterValue = document.querySelector(".counter-value");
+let counteur = 0;
+let min = 0;
+let max = 11;
+
+
+// reset.addEventListener('click', function(){
+//     if(counteur == min.value && max.value)
+// })
+decrement.addEventListener('click', function(){
+    //console.log(counterValue.innerText);
+    if(counteur > min){
+        counterValue.innerText = counteur;
+        counteur --;
+    };
+});
+
+increse.addEventListener("click", function(){
+    if(counteur < max){
+        counterValue.innerText = counteur;
+        counteur ++;
+    }
+});
+
+
+
+
